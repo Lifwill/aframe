7f997f61d85e213d796eb7859089c43318fd6f43
Jonathan Wafellman <jwafellman@gmail.com>
2022-02-18 16:08:49 +0100

    Work in progress, set up the server configuration + some tries on the events

---
+++ src/application.ts
@@ -0,0 +1,24 @@
+
+AFRAME.registerComponent('start', {
+
+    init: function () {
+        /* this.el.innerHTML = `
+         <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
+         <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+         <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
+         <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+         <a-sky color="#ECECEC"></a-sky>`;*/
+        /*
+                const node = document.createElement('a-box');
+                let position = -3;
+                node.setAttribute('position', '-1 0.5 -3');
+                node.setAttribute('color', '#4CC3D9');
+                node.setAttribute('rotation', '0 45 0');
+                this.el.appendChild(node);
+        
+                setInterval(() => {
+                    position = position - 1;
+                    node.setAttribute('position', `-1 0.5 ${position}`);
+                }, 1000)*/
+    }
+});
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
-import './style.css'
+import 'aframe';
+import 'aframe-event-set-component';
+import "./application";
+import "./events/thumbstick";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
-
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+document.body.innerHTML = `<a-scene id="scene" start="">
+<a-entity hand-controls="left"></a-entity>
+<a-entity hand-controls="right"></a-entity>
+<a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
+<a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+<a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"
+event-set__enter="_event: mouseenter; color: #8FF7FF"
+event-set__leave="_event: mouseleave; color: #4CC3D9"></a-cylinder>
+<a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+<a-plane position="4 0 -4" rotation="-90 0 0" width="4" height="4" color="#EF2D5E"></a-plane>
+<a-plane position="8 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+<a-plane position="12 0 -4" rotation="-90 0 0" width="4" height="4" color="#EF2D5E"></a-plane>
+<a-plane position="0 0 0" rotation="-90 0 0" width="4" height="4" color="#EF2D5E"></a-plane>
+<a-plane position="4 0 0" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+<a-plane position="8 0 0" rotation="-90 0 0" width="4" height="4" color="#EF2D5E"></a-plane>
+<a-plane position="12 0 0" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+<a-sky color="#ECECEC"></a-sky>
+<a-entity oculus-touch-controls="hand: left" thumbstick-logging></a-entity>
+<a-entity oculus-touch-controls="hand: right" thumbstick-logging></a-entity>
+<a-camera>
+  <a-cursor>
+  </a-cursor>
+</a-camera>
+</a-scene>`;
